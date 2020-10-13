48faecfca4ff755bc4637a85497ed130a4ce573a
matt-mekha <mtmekha8@gmail.com>
2020-10-12 20:55:51 -0500

    finished bubbles

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 
 import Title from '../components/Title';
 
-const BubbleVisualizer = () => {
-  const size = 600;
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+}
 
+const BubbleVisualizer = () => {
+  const [width, height] = useWindowSize();
   return (
     <div className="bubbleVisualizerContainer">
-      <iframe className="bubbleVisualizer" src="misc/bubbles.html"></iframe>
+      <iframe title="Meet the Team" className="bubbleVisualizer" src={`misc/bubbles.html?w=${width}&h=${height}`}></iframe>
     </div>
   );
 }
