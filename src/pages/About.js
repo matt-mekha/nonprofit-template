453115fb390c365214478b072bfbd552fec5d6b0
matt-mekha <mtmekha8@gmail.com>
2020-10-13 13:11:13 -0500

    events page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React from 'react';
 
 import Title from '../components/Title';
 
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,5 +1,35 @@
 import React from 'react';
 
+import Title from '../components/Title';
+
+// const useWindowSize = () => {
+//   const [size, setSize] = useState([0, 0]);
+//   useLayoutEffect(() => {
+//     function updateSize() {
+//       setSize([window.innerWidth, window.innerHeight]);
+//     }
+//     window.addEventListener('resize', updateSize);
+//     updateSize();
+//     return () => window.removeEventListener('resize', updateSize);
+//   }, []);
+//   return size;
+// }
+
+const CalendarEmbed = () => {
+  const height = 600;
+  
+  return (<iframe
+    title="Calendar"
+    src={`https://calendar.google.com/calendar/embed?height=${height}&wkst=1&bgcolor=%237986CB&ctz=America%2FChicago&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%230B8043&showTz=0&showTitle=0&showNav=0&showDate=0&showTabs=0&mode=AGENDA&showCalendars=0&showPrint=0&title=Embed%20Example`}
+    frameBorder="0"
+    scrolling="no"
+    className="calendarEmbed"
+  ></iframe>);
+}
+
 export default () => (
-  <p>events page goes here</p>
+  <>
+    <Title>Events Coming Up:</Title>
+    <CalendarEmbed />
+  </>
 )
