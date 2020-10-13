17efa809e85bcfb00f1c2e15b28b9d731078dafa
matt-mekha <mtmekha8@gmail.com>
2020-10-13 09:50:26 -0500

    fixed bubble reloading

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,24 +2,25 @@ import React, { useLayoutEffect, useState } from 'react';
 
 import Title from '../components/Title';
 
-const useWindowSize = () => {
-  const [size, setSize] = useState([0, 0]);
-  useLayoutEffect(() => {
-    function updateSize() {
-      setSize([window.innerWidth, window.innerHeight]);
-    }
-    window.addEventListener('resize', updateSize);
-    updateSize();
-    return () => window.removeEventListener('resize', updateSize);
-  }, []);
-  return size;
-}
+// const useWindowSize = () => {
+//   const [size, setSize] = useState([0, 0]);
+//   useLayoutEffect(() => {
+//     function updateSize() {
+//       setSize([window.innerWidth, window.innerHeight]);
+//     }
+//     window.addEventListener('resize', updateSize);
+//     updateSize();
+//     return () => window.removeEventListener('resize', updateSize);
+//   }, []);
+//   return size;
+// }
 
 const BubbleVisualizer = () => {
-  const [width, height] = useWindowSize();
+  //const [width, height] = useWindowSize();
+  //<iframe title="Meet the Team" className="bubbleVisualizer" src={`misc/bubbles.html?w=${width}&h=${height}`}></iframe>
   return (
     <div className="bubbleVisualizerContainer">
-      <iframe title="Meet the Team" className="bubbleVisualizer" src={`misc/bubbles.html?w=${width}&h=${height}`}></iframe>
+      <iframe title="Meet the Team" className="bubbleVisualizer" src="misc/bubbles.html"></iframe>
     </div>
   );
 }
