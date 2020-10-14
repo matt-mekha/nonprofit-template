0a30aa1d95c617cbf7bbc130f50255aba93c0f3f
matt-mekha <mtmekha8@gmail.com>
2020-10-14 13:11:57 -0500

    donate page

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,5 +1,27 @@
 import React from 'react';
 
+import Title from '../components/Title';
+import { donationEmbedLink } from "../data/info.json"
+
+const DonorboxEmbed = () => (
+  <>
+    <script src="https://donorbox.org/widget.js" paypalExpress="false"></script>
+    <iframe
+      title="Donorbox Embed"
+      allowpaymentrequest=""
+      frameborder="0"
+      name="donorbox"
+      scrolling="no"
+      seamless="seamless"
+      src={donationEmbedLink}
+      className="donorboxEmbed"
+    ></iframe>
+  </>
+)
+
 export default () => (
-  <p>donate page goes here</p>
+  <>
+    <Title>Donate:</Title>
+    <DonorboxEmbed />
+  </>
 )
